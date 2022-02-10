c7831bcc2f659b3d09d3f8d4ed6d57deb9242591
will-da-pro <william@dolier.net>
2022-02-10 18:48:57 +1100

    Bug fixes

--- src/me/snowzen/snowzenresources/skills/Skills.java
+++ src/me/snowzen/snowzenresources/skills/Skills.java
@@ -17,53 +17,61 @@ public class Skills implements Listener {
 		}
 		Material block = br.getBlock().getType();
 		if (block != Material.STONE & 
-			 block != Material.COAL_ORE & 
+			 block != Material.COAL_ORE &
+			 block != Material.DEEPSLATE_COAL_ORE &
+			 block != Material.DEEPSLATE_IRON_ORE &
 			 block != Material.IRON_ORE & 
 			 block != Material.REDSTONE_ORE & 
+			 block != Material.DEEPSLATE_REDSTONE_ORE &
 			 block != Material.GOLD_ORE & 
+			 block != Material.DEEPSLATE_GOLD_ORE &
+			 block != Material.DEEPSLATE_DIAMOND_ORE &
 			 block != Material.DIAMOND_ORE & 
 			 block != Material.EMERALD_ORE & 
+			 block != Material.DEEPSLATE_EMERALD_ORE &
 			 block != Material.DIORITE & 
-			 block != Material.GRANITE ) {
+			 block != Material.GRANITE &
+			 block != Material.ANDESITE ) {
 			br.getPlayer().sendMessage("Wrong Block");
 		 return;
 		}
 		int stone = 1, deepslate = 5, diorite = 2, granite = 2, andesite = 2, coal = 5, iron = 15, gold = 30, redstone = 20, diamond = 50, emerald = 70;
 		Player player = br.getPlayer();
+		player.sendMessage("players." + player.getName() + ".skills.mining.xp");
 	 
-		int xp = Main.plugin.getConfig().getInt("players." + player.getName() + ".skills.mining.xp");
+		Integer xp = Main.plugin.getConfig().getInt("players." + player.getName() + ".skills.mining.xp");
 		if (block == Material.STONE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + stone);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + stone);
 		}
 		else if (block == Material.DEEPSLATE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + deepslate);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + deepslate);
 		}
 		else if (block == Material.DIORITE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + diorite);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + diorite);
 		}
 		else if (block == Material.ANDESITE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + andesite);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + andesite);
 		}
 		else if (block == Material.GRANITE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + granite);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + granite);
 		}
 		else if (block == Material.COAL_ORE || block == Material.DEEPSLATE_COAL_ORE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + coal);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + coal);
 		}
 		else if (block == Material.IRON_ORE || block == Material.DEEPSLATE_IRON_ORE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + iron);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + iron);
 		}
 		else if (block == Material.REDSTONE_ORE || block == Material.DEEPSLATE_REDSTONE_ORE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + redstone);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + redstone);
 		}
 		else if (block == Material.GOLD_ORE || block == Material.DEEPSLATE_GOLD_ORE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + gold);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + gold);
 		}
 		else if (block == Material.DIAMOND_ORE || block == Material.DEEPSLATE_DIAMOND_ORE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + diamond);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + diamond);
 		}
 		else if (block == Material.EMERALD_ORE || block == Material.DEEPSLATE_EMERALD_ORE) {
-			Main.plugin.getConfig().set("players." + player.getDisplayName() + ".skills.mining.xp", xp + emerald);
+			Main.plugin.getConfig().set("players." + player.getName() + ".skills.mining.xp", xp + emerald);
 		}
 		player.sendMessage("Mined 1 " + block.name());
 		player.sendMessage(Integer.toString(xp));
