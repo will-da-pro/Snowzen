e14dd309129464260132f959322f0632dd7ea181
will-da-pro <william@dolier.net>
2022-02-24 17:58:43 +1100

    mythic mobs

---
+++ src/me/snowzen/snowzenresources/Mobs/Mobs.java
@@ -0,0 +1,11 @@
+package me.snowzen.snowzenresources.Mobs;
+
+import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
+import io.lumine.xikage.mythicmobs.mobs.MythicMob;
+
+public class Mobs {
+	public void newMob() {
+		ActiveMob mob = new ActiveMob();
+		mob.setEntity("CREEPER");
+	}
+}
